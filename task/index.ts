9e4ad6f639292a6bcfacceb72dffee342eb7a97d
Bianca Rosa <me@biancarosa.com.br>
2020-06-08 20:46:27 -0300

    finish push secrets code

--- task/index.ts
+++ task/index.ts
@@ -1,34 +1,49 @@
 import tl = require("azure-pipelines-task-lib/task");
-const {SecretManagerServiceClient} = require('@google-cloud/secret-manager'); 
+const { SecretManagerServiceClient } = require("@google-cloud/secret-manager");
 const client = new SecretManagerServiceClient();
 
 async function run() {
-    let prefix = tl.getInput("prefix", false) || ""
-    let project = tl.getInput("project", true) || ""
-    let pushSecrets = tl.getBoolInput("pushSecret", true) || false
+  let prefix = tl.getInput("prefix", false) || "";
+  let project = tl.getInput("project", true) || "";
+  let pushSecrets = tl.getBoolInput("pushSecret", true) || false;
 
-    try {
-        if (pushSecrets) {
-            tl.getVariables().forEach((variable) => {
-                if (variable.secret) {
-                    let secretId = `${prefix}${variable.name}`;
-                    const [secret] = client.createSecret({
-                    parent: `projects/${project}`,
-                    secret: {
-                        name: secretId,
-                        replication: {
-                        automatic: {},
-                        },
-                    },
-                    secretId,
-                    });
-                    console.info(`Created secret ${secret.name}`);
-                }
+  try {
+    if (pushSecrets) {
+      tl.getVariables().forEach((variable) => {
+        if (variable.secret) {
+          let secretId = `${prefix}${variable.name}`;
+          let [secret] = client.getSecret({
+            parent: parent,
+            secret: secretId,
+          });
+          if (secret) {
+            console.info(`Already found ${secret.name}`);
+          } else {
+            [secret] = client.createSecret({
+              parent: `projects/${project}`,
+              secret: {
+                name: secretId,
+                replication: {
+                  automatic: {},
+                },
+              },
+              secretId,
             });
-      } 
-    } catch (err) {
-        tl.setResult(tl.TaskResult.Failed, err.message);
+            console.info(`Created secret ${secret.name}`);
+          }
+          const [version] = client.addSecretVersion({
+            parent: secret.name,
+            payload: {
+              data: Buffer.from(variable.value, "utf8"),
+            },
+          });
+          console.info(`Added secret version ${version.name}`);
+        }
+      });
     }
+  } catch (err) {
+    tl.setResult(tl.TaskResult.Failed, err.message);
+  }
 }
 
 run();
