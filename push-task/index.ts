233661a95fd66df2e5cb92597e4ed81244588c9c
Bianca Rosa <me@biancarosa.com.br>
2020-06-25 14:35:43 -0300

    remove push secrets input

--- push-task/index.ts
+++ push-task/index.ts
@@ -5,44 +5,41 @@ const client = new SecretManagerServiceClient();
 async function run() {
   let prefix = tl.getInput("prefix", false) || "";
   let project = tl.getInput("project", true) || "";
-  let pushSecrets = tl.getBoolInput("pushSecrets", true) || false;
 
   try {
-    if (pushSecrets) {
-      console.log("Pushing secrets to GSM")
-      tl.getVariables().forEach((variable) => {
-        if (variable.secret) {
-          console.log("Push", variable.name)
-          let secretId = `${prefix}${variable.name}`;
-          let [secret] = client.getSecret({
-            parent: parent,
-            secret: secretId,
-          });
-          if (secret) {
-            console.info(`Already found ${secret.name}`);
-          } else {
-            [secret] = client.createSecret({
-              parent: `projects/${project}`,
-              secret: {
-                name: secretId,
-                replication: {
-                  automatic: {},
-                },
+    console.log("Pushing secrets to GSM")
+    tl.getVariables().forEach((variable) => {
+      if (variable.secret) {
+        console.log("Push", variable.name)
+        let secretId = `${prefix}${variable.name}`;
+        let [secret] = client.getSecret({
+          parent: parent,
+          secret: secretId,
+        });
+        if (secret) {
+          console.info(`Already found ${secret.name}`);
+        } else {
+          [secret] = client.createSecret({
+            parent: `projects/${project}`,
+            secret: {
+              name: secretId,
+              replication: {
+                automatic: {},
               },
-              secretId,
-            });
-            console.info(`Created secret ${secret.name}`);
-          }
-          const [version] = client.addSecretVersion({
-            parent: secret.name,
-            payload: {
-              data: Buffer.from(variable.value, "utf8"),
             },
+            secretId,
           });
-          console.info(`Added secret version ${version.name}`);
+          console.info(`Created secret ${secret.name}`);
         }
-      });
-    }
+        const [version] = client.addSecretVersion({
+          parent: secret.name,
+          payload: {
+            data: Buffer.from(variable.value, "utf8"),
+          },
+        });
+        console.info(`Added secret version ${version.name}`);
+      }
+    });
   } catch (err) {
     tl.setResult(tl.TaskResult.Failed, err.message);
   }
