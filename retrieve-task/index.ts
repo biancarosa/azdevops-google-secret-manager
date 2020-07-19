d21bc370b398ab2d2ff5a185a80f5cfcc2a2c9c3
Bianca Rosa <me@biancarosa.com.br>
2020-07-19 20:16:16 -0300

    start  code for secret retrieval

---
+++ retrieve-task/index.ts
@@ -0,0 +1,27 @@
+import tl = require("azure-pipelines-task-lib/task");
+const { SecretManagerServiceClient } = require("@google-cloud/secret-manager");
+const client = new SecretManagerServiceClient();
+
+async function run() {
+  let project = tl.getInput("project", true) || "";
+
+  try {
+    console.log("Retrieving secrets from GSM")
+    const [secrets] = await client.listSecrets({
+      parent: `projects/${project}`,
+    });
+    secrets.forEach(async (secret: any) => {
+      const [versions] = await client.listSecretVersions({
+        parent: parent,
+      });
+      const [lastVersion] = await client.accessSecretVersion({
+        name: versions[0].name
+      })
+      tl.setVariable(secret.name.split('/secrets/')[1], lastVersion.payload.data.toString(), true);
+    });
+  } catch (err) {
+    tl.setResult(tl.TaskResult.Failed, err.message);
+  }
+}
+
+run();
---
+++ retrieve-task/tests/_suite.ts
@@ -0,0 +1,10 @@
+import * as path from "path";
+import * as assert from "assert";
+import * as ttm from "azure-pipelines-task-lib/mock-test";
+import fs = require("fs");
+
+describe("Sample task tests", function () {
+  before(function () {});
+
+  after(() => {});
+});
