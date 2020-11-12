712ee6eae1ba9896b5a512ece328b5cb9377f111
Bianca Mendonça | Stone <bmendonca@stone.com.br>
2020-11-12 15:32:32 -0300

    Add more logs

--- retrieve-task/index.ts
+++ retrieve-task/index.ts
@@ -11,13 +11,19 @@ async function run() {
       parent: `projects/${project}`,
     });
     secrets.forEach(async (secret: any) => {
-      const [versions] = await client.listSecretVersions({
-        parent: secret.name,
-      });
-      const [lastVersion] = await client.accessSecretVersion({
-        name: versions[0].name
-      })
-      tl.setVariable(secret.name.split('/secrets/')[1], lastVersion.payload.data.toString(), true);
+      try {
+        console.log(`Listing versions for secret ${secret.name}`)
+        const [versions] = await client.listSecretVersions({
+          parent: secret.name,
+        });
+        console.log(`Acession version value for secret ${secret.name} and version ${versions[0].name}`)
+        const [lastVersion] = await client.accessSecretVersion({
+          name: versions[0].name
+        })
+        tl.setVariable(secret.name.split('/secrets/')[1], lastVersion.payload.data.toString(), true);
+      } catch (err) {
+        console.error(err);
+      }
     });
   } catch (err) {
     tl.setResult(tl.TaskResult.Failed, "Input required: project");
